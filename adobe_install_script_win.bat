5cb4c11a1564c5ac47535115890a370b1409d350
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-08-25 11:03:06 -0700

    Final commit for adobe activation
    Renamed file. Now the script is generic and can be ran for adobe pro,
    adobe novid or adobe full. I've already tested this with adobe pro and
    adobe novid and it works.

--- adobe_install_script_win.bat
+++ adobe_install_script_win.bat
@@ -2,17 +2,17 @@ rem Set up the Windows path so that WinRAR can be run more easily from a script
 rem then extract the adobe installer from the zip.
 
 setx path "%path%;C:\Program Files\WinRAR"
-winrar x cc_win_Acrobat_x64_2014-09.zip .
+winrar x cc_win_*.zip .
 
-rem Install Adobe Pro.
-cd cc_win_Acrobat_x64*
+rem Install Adobe.
+cd cc_win*
 .\Exceptions\ExceptionDeployer.exe --workflow=install --mode=pre
 cd Build
 setup.exe
 cd..
 .\Exceptions\ExceptionDeployer.exe --workflow=install --mode=post
 cd ..
-.\cc_serialize2014\win\adobe_prtk.exe --tool=VolumeSerialize --provfile=.\cc_serialize2014\win\prov_win.xml
+.\cc_serialize*\win\adobe_prtk.exe --tool=VolumeSerialize --provfile=.\cc_serialize*\win\prov_win.xml
 
 echo DONE!
 pause
