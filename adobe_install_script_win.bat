fd2eea7f00cb2798cf7d26a292efaad444846feb
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-08-27 10:42:42 -0700

    Minor Update
    Now checks for WinRAR before attempting the installation. It also now
    unzips the serialization stuff as well.

--- adobe_install_script_win.bat
+++ adobe_install_script_win.bat
@@ -1,7 +1,16 @@
+@echo off
 rem Set up the Windows path so that WinRAR can be run more easily from a script
 rem then extract the adobe installer from the zip.
 
+if not exist "C:\Program Files\WinRAR\winrar.exe" (
+	echo You need to have WinRAR installed to run this install script.
+	pause
+	exit
+) else (echo WinRAR is installed.)
+
+rem unzip files.
 "C:\Program Files\WinRAR\winrar.exe" x win_cc*.zip .
+"C:\Program Files\WinRAR\winrar.exe" x cc20*win.zip .
 
 rem Install Adobe.
 cd win_cc*
