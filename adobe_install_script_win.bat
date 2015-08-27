fb2f620bb081d3efa437b9d99f5674e75e8c57e3
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-08-27 10:05:58 -0700

    Update for Adobe Creative Cloud 2015

--- adobe_install_script_win.bat
+++ adobe_install_script_win.bat
@@ -1,18 +1,17 @@
 rem Set up the Windows path so that WinRAR can be run more easily from a script
 rem then extract the adobe installer from the zip.
 
-setx path "%path%;C:\Program Files\WinRAR"
-winrar x cc_win_*.zip .
+"C:\Program Files\WinRAR\winrar.exe" x win_cc*.zip .
 
 rem Install Adobe.
-cd cc_win*
+cd win_cc*
 .\Exceptions\ExceptionDeployer.exe --workflow=install --mode=pre
 cd Build
 setup.exe
 cd..
 .\Exceptions\ExceptionDeployer.exe --workflow=install --mode=post
 cd ..
-.\cc_serialize*\win\adobe_prtk.exe --tool=VolumeSerialize --provfile=.\cc_serialize*\win\prov_win.xml
+.\cc*serializ*\win\adobe_prtk.exe --tool=VolumeSerialize --provfile=.\cc*serializ*\win\prov_win.xml
 
 echo DONE!
 pause
